43f8455011c2ced98332a1768234a55eecc59e66
baejeu <zeus20240228@gmail.com>
2025-03-15 14:19:07 +0900

    [2025:03:15] Testcontainers TDD 공부 프로젝트 생성

---
+++ testcontainers/src/test/java/spring/testcontainers/TestTestcontainersApplication.java
@@ -0,0 +1,11 @@
+package spring.testcontainers;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTestcontainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TestcontainersApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
